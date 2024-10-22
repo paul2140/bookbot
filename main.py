bb99493ba0db6efd35937bbc96fd21e79ffa51ed
git_PaulW <starsrift@netscape.net>
2024-10-22 02:42:44 -0700

    Completion of task

---
+++ main.py
@@ -0,0 +1,33 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f" The number of words in the book was {num_words}")
+    chars_dict = get_chars_dict(text)
+    #print(chars_dict)
+    for i in chars_dict:
+        print(f"The numbers of words in {i} is {chars_dict[i]}")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
